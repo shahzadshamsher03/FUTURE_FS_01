ad34dd9a333560176e3f6307c530dc859d9b9c09
shahzadshamsher03 <shahzadshamsher03@gmail.com>
2026-01-22 18:06:14 +0530

    first commit

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,89 @@
+import { Briefcase, Code, FileCode, FileCode2, FileCode2Icon, FileCodeCornerIcon, LucideFileCode, Terminal, User, Users } from "lucide-react";
+
+export const AboutMe = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Web Developer & Tech Creator
+            </h3>
+
+            <p className="text-muted-foreground">
+              I’m passionate about building elegant solutions to real-world problems and continuously learning new technologies to stay current in the fast-evolving web ecosystem.
+            </p>
+
+            <p className="text-muted-foreground">
+              I enjoy solving problems by breaking them down into simple, efficient solutions and continuously improving my logical thinking skills.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="#" target="_blank"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating responsive websites and web applications with
+                    modern frameworks.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <FileCode2Icon className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Problem Solving</h4>
+                  <p className="text-muted-foreground">
+                    Designing intuitive user interfaces and seamless user
+                    experiences.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Users className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Team Player</h4>
+                  <p className="text-muted-foreground">
+                    I enjoy working in teams, learning from others, and contributing my skills to build projects together successfully.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import {StarBackground} from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import {HeroSection} from "../components/HeroSection"
+import { AboutMe } from "../components/AboutMe";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectSection";
+import {ContactSection} from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+
+export const Home =()=>{
+    return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+     {/* Theme Toggle */ }   
+     <ThemeToggle/>
+
+     {/*Background Effects*/}
+     <StarBackground/>
+
+     {/* Navbar*/}
+     <Navbar/>
+
+     {/*Main Content*/}
+     <main>
+        <HeroSection/>
+        <AboutMe/>
+        <SkillsSection/>
+        <ProjectsSection/>
+        <ContactSection/>
+     </main>
+
+     {/*Footer*/}
+    <Footer/>
+
+
+    </div>
+    );
+}
